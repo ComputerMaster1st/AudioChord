e615ae34455f7411aecf0934112a12d1a1873e55
ComputerMaster1st <mr_m.a_thompson@hotmail.co.uk>
2018-02-04 20:25:35 +0000

    Added GetSongAsync

--- src/Shared.Music/Providers/Storage/GridFS/GridFSProvider.cs
+++ src/Shared.Music/Providers/Storage/GridFS/GridFSProvider.cs
@@ -1,10 +1,32 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿using MongoDB.Driver;
+using MongoDB.Driver.GridFS;
+using System;
+using System.IO;
+using System.Threading.Tasks;
 
 namespace Shared.Music.Providers.Storage.GridFS
 {
     internal class GridFSProvider : IStorageProvider
     {
+        private GridFSBucket bucket;
+
+        public GridFSProvider(IMongoDatabase database)
+        {
+            bucket = new GridFSBucket(database, new GridFSBucketOptions()
+            {
+                BucketName = "OpusFiles",
+                ChunkSizeBytes = 2097152
+            });
+        }
+
+        public Task<Playlist> GetPlaylistAsync(Guid id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<Stream> GetSongAsync(Guid id)
+        {
+            return await bucket.OpenDownloadStreamByNameAsync($"{id}.opus");
+        }
     }
 }
