91245b38c120379ea52f0a752fe887e678b1cd79
ComputerMaster1st <mr_m.a_thompson@hotmail.co.uk>
2018-02-05 16:37:16 +0000

    Initialized database connection

--- src/Shared.Music/MusicService.cs
+++ src/Shared.Music/MusicService.cs
@@ -1,12 +1,15 @@
-﻿namespace Shared.Music
+﻿using MongoDB.Driver;
+
+namespace Shared.Music
 {
     public class MusicService
     {
-        private MusicServiceConfig config;
+        private IMongoDatabase database;
 
         public MusicService(MusicServiceConfig config)
         {
-            this.config = config;
+            MongoClient client = new MongoClient($"mongodb://{config.Username}:{config.Password}@localhost:27017/sharedmusic");
+            database = client.GetDatabase("sharedmusic");
         }
     }
 }
