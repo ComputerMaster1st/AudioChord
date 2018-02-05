74653119fda6111050d5424e8cd11f9569f7cdf4
ComputerMaster1st <mr_m.a_thompson@hotmail.co.uk>
2018-02-05 17:18:04 +0000

    Removed getsong method

--- src/Shared.Music/Collections/Models/Playlist.cs
+++ src/Shared.Music/Collections/Models/Playlist.cs
@@ -3,19 +3,9 @@ using System.Collections.Generic;
 
 namespace Shared.Music.Collections.Models
 {
-    internal class Playlist : List<SongMeta>
+    internal class Playlist : List<Guid>
     {
         public Guid Id { get; private set; } = new Guid();
         public string Name { get; private set; }
-
-        public SongMeta GetSong(Guid Id)
-        {
-            foreach (SongMeta meta in this)
-            {
-                if (meta.Id == Id) return meta;
-            }
-
-            return null;
-        }
     }
 }
