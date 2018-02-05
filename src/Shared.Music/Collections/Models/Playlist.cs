c0dd6ef32939d33ea259fd2808a7a9e0cc640da1
ComputerMaster1st <mr_m.a_thompson@hotmail.co.uk>
2018-02-05 17:13:04 +0000

    Added GetSong to Playlist

--- src/Shared.Music/Collections/Models/Playlist.cs
+++ src/Shared.Music/Collections/Models/Playlist.cs
@@ -1,10 +1,21 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace Shared.Music.Collections.Models
 {
-    internal class Playlist
+    internal class Playlist : List<SongMeta>
     {
         public Guid Id { get; private set; } = new Guid();
         public string Name { get; private set; }
+
+        public SongMeta GetSong(Guid Id)
+        {
+            foreach (SongMeta meta in this)
+            {
+                if (meta.Id == Id) return meta;
+            }
+
+            return null;
+        }
     }
 }
