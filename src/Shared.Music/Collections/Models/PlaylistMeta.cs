a3f3d7acf96554a1c4ec02f60b1ed34f23a999d7
ComputerMaster1st <mr_m.a_thompson@hotmail.co.uk>
2018-02-05 17:02:18 +0000

    Initialized PlaylistCollection & moved data types to Models folder within collections

---
+++ src/Shared.Music/Collections/Models/PlaylistMeta.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace Shared.Music.Collections
+{
+    internal class PlaylistMeta
+    {
+        public Guid Id { get; private set; } = new Guid();
+        public string Name { get; private set; }
+    }
+}
---
+++ src/Shared.Music/Collections/PlaylistCollection.cs
@@ -0,0 +1,14 @@
+﻿using MongoDB.Driver;
+
+namespace Shared.Music.Collections
+{
+    internal class PlaylistCollection
+    {
+        private IMongoCollection<PlaylistMeta> collection;
+
+        internal PlaylistCollection(IMongoCollection<PlaylistMeta> collection)
+        {
+            this.collection = collection;
+        }
+    }
+}
