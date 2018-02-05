cffc9a06bb06c4cfe778ae403cde9ab076d2c430
ComputerMaster1st <mr_m.a_thompson@hotmail.co.uk>
2018-02-05 16:25:08 +0000

    Initialized CommandService & CommandServiceConfig

---
+++ src/Shared.Music/CommandServiceConfig.cs
@@ -0,0 +1,8 @@
+﻿namespace Shared.Music
+{
+    public class CommandServiceConfig
+    {
+        public string Username { get; set; }
+        public string Password { get; set; }
+    }
+}
---
+++ src/Shared.Music/MusicService.cs
@@ -0,0 +1,12 @@
+﻿namespace Shared.Music
+{
+    public class MusicService
+    {
+        private CommandServiceConfig config;
+
+        public MusicService(CommandServiceConfig config)
+        {
+            this.config = config;
+        }
+    }
+}
