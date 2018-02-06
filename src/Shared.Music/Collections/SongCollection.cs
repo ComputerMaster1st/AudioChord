6a585a797f1b5304ea003a402de733c8d3ae03c2
ComputerMaster1st <mr_m.a_thompson@hotmail.co.uk>
2018-02-06 16:51:53 +0000

    Added GetAsync

--- src/Shared.Music/Collections/SongCollection.cs
+++ src/Shared.Music/Collections/SongCollection.cs
@@ -1,5 +1,7 @@
 ﻿using MongoDB.Driver;
 using Shared.Music.Collections.Models;
+using System;
+using System.Threading.Tasks;
 
 namespace Shared.Music.Collections
 {
@@ -10,6 +12,12 @@ namespace Shared.Music.Collections
         internal SongCollection(IMongoCollection<Song> collection)
         {
             this.collection = collection;
+        }
+
+        public async Task<Song> GetAsync(Guid Id)
+        {
+            var result = await collection.FindAsync((f) => f.Id.Equals(Id));
+            return await result.FirstOrDefaultAsync();
         }
     }
 }
