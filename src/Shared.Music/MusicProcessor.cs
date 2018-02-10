c0ce44593e324b2a070086809286d6aaa934417f
ComputerMaster1st <mr_m.a_thompson@hotmail.co.uk>
2018-02-10 14:14:40 +0000

    Added ObtainVideoAsync

--- src/Shared.Music/MusicProcessor.cs
+++ src/Shared.Music/MusicProcessor.cs
@@ -1,6 +1,41 @@
-﻿namespace Shared.Music
+﻿using System;
+using System.Threading.Tasks;
+using YoutubeExplode;
+using YoutubeExplode.Models;
+using YoutubeExplode.Models.MediaStreams;
+
+namespace Shared.Music
 {
     internal class MusicProcessor
     {
+        private YoutubeClient Client = new YoutubeClient();
+        private string VideoId;
+        private string Name;
+        private TimeSpan Length;
+        private string Author;
+        private AudioStreamInfo StreamInfo;
+
+        internal MusicProcessor(string Url)
+        {
+            VideoId = YoutubeClient.ParseVideoId(Url);
+        }
+
+        internal async Task<bool> ObtainVideoAsync()
+        {
+            try
+            {
+                Video VInfo = await Client.GetVideoAsync(VideoId);
+
+                Name = VInfo.Title;
+                Length = VInfo.Duration;
+                Author = VInfo.Author;
+
+                return true;
+            } catch
+            {
+                return false;
+            }
+
+        }
     }
 }
