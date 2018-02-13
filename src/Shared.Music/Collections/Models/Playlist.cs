450a6c3bdcc78de3a1adfb5a936e5d2092abfbda
ComputerMaster1st <mr_m.a_thompson@hotmail.co.uk>
2018-02-13 18:02:41 +0000

    Minor changes

--- src/Shared.Music/Collections/Models/Playlist.cs
+++ src/Shared.Music/Collections/Models/Playlist.cs
@@ -1,12 +1,12 @@
-﻿using MongoDB.Bson.Serialization.Attributes;
-using System;
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
 using System.Collections.Generic;
 
 namespace Shared.Music.Collections.Models
 {
     public class Playlist
     {
-        [BsonId] public Guid Id { get; internal set; } = new Guid();
-        public List<Guid> SongList { get; internal set; } = new List<Guid>();
+        [BsonId] public ObjectId Id { get; internal set; } = new ObjectId();
+        public List<ObjectId> SongList { get; internal set; } = new List<ObjectId>();
     }
 }
