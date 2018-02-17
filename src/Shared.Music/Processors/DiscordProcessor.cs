d6a71d4b152c083886c68ceb4cfdf88aea4bc4a0
ComputerMaster1st <mr_m.a_thompson@hotmail.co.uk>
2018-02-17 23:44:11 +0000

    Started on DiscordProcessor

---
+++ src/Shared.Music/Processors/DiscordProcessor.cs
@@ -0,0 +1,40 @@
+﻿using Shared.Music.Collections.Models;
+using System;
+using System.IO;
+using System.Net;
+using System.Threading.Tasks;
+
+namespace Shared.Music.Processors
+{
+    /// <summary>
+    /// Convert discord url links to opus audio data
+    /// </summary>
+    internal class DiscordProcessor
+    {
+        private WebClient client = new WebClient();
+        private FFMpegEncoder encoder = new FFMpegEncoder();
+
+        public string AttachmentId { get; private set; }
+        public SongMeta Metadata { get; private set; }
+
+        internal static async Task<DiscordProcessor> RetrieveAsync(string url)
+        {
+            if (string.IsNullOrWhiteSpace(url))
+                throw new ArgumentNullException("The url given is either null or empty!");
+
+            DiscordProcessor processor = new DiscordProcessor();
+
+            return processor;
+        }
+
+        private async Task GetMetadataAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        internal async Task<Stream> ProcessAudioAsync()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
