aa8b7331563573c7f37abc2f92ce93a367a3836e
ComputerMaster1st <mr_m.a_thompson@hotmail.co.uk>
2018-02-18 13:21:37 +0000

    Added file probing

--- src/Shared.Music/Processors/DiscordProcessor.cs
+++ src/Shared.Music/Processors/DiscordProcessor.cs
@@ -1,5 +1,8 @@
-﻿using Shared.Music.Collections.Models;
+﻿using Newtonsoft.Json;
+using Shared.Music.Collections.Models;
 using System;
+using System.Collections.Generic;
+using System.Diagnostics;
 using System.IO;
 using System.Net;
 using System.Threading.Tasks;
@@ -24,13 +27,52 @@ namespace Shared.Music.Processors
 
             DiscordProcessor processor = new DiscordProcessor();
 
+            await processor.GetMetadataAsync(url);
+
             return processor;
         }
 
-        private async Task GetMetadataAsync()
+        private async Task<Dictionary<string, dynamic>> ProbeFileAsync(string filename)
         {
-            // TODO: ffprobe -i wow_its_so_happy.ogg -hide_banner -show_format -print_format json -v quiet > test.json
-            throw new NotImplementedException();
+            TaskCompletionSource<int> awaitExitSource = new TaskCompletionSource<int>();
+            string json;
+
+            using (Process process = new Process()
+            {
+                StartInfo = new ProcessStartInfo()
+                {
+                    FileName = "ffmpeg",
+                    Arguments = $"-i {filename} -hide_banner -show_format -print_format json -v quiet",
+                    UseShellExecute = false,
+                    RedirectStandardError = true
+                },
+                EnableRaisingEvents = true
+            })
+            {
+                process.Exited += (obj, args) => {
+                    awaitExitSource.SetResult(process.ExitCode);
+                };
+
+                process.Start();
+
+                json = await process.StandardError.ReadToEndAsync();
+                int exitCode = await awaitExitSource.Task;
+            }
+
+            return JsonConvert.DeserializeObject<Dictionary<string, dynamic>>(json);
+        }
+
+        private async Task GetMetadataAsync(string url)
+        {
+            // TODO: ffprobe -i wow_its_so_happy.ogg  > test.json
+            if (!Uri.TryCreate(url, UriKind.Absolute, out Uri result))
+                throw new ArgumentException("Url Provided is invalid!");
+
+            string filename = url.Substring(url.LastIndexOf('/'));
+
+            client.DownloadFileAsync(result, filename);
+
+            Dictionary<string, dynamic> probe = await ProbeFileAsync(filename);
         }
 
         internal async Task<Stream> ProcessAudioAsync()
