55703e8a95973bc8aa02d7dfcbd11aef1d560cf0
ComputerMaster1st <mr_m.a_thompson@hotmail.co.uk>
2018-02-22 13:49:45 +0000

    Added Url property

--- src/Shared.Music/Collections/Models/SongMetadata.cs
+++ src/Shared.Music/Collections/Models/SongMetadata.cs
@@ -7,12 +7,14 @@ namespace Shared.Music.Collections.Models
         public string Name { get; private set; }
         public TimeSpan Length { get; private set; }
         public string Uploader { get; private set; }
+        public string Url { get; private set; }
 
-        internal SongMetadata(string name, TimeSpan length, string uploader)
+        internal SongMetadata(string name, TimeSpan length, string uploader, string url)
         {
             Name = name;
             Length = length;
             Uploader = uploader;
+            Url = url;
         }
     }
 }
