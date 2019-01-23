01c436ae817ae9315da16decd3ca5a302f8b83de
Lucas Spits <spits.lucas@gmail.com>
2019-01-23 15:22:17 +0100

    Add a cache interface

---
+++ src/AudioChord/Caching/ISongCache.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace AudioChord.Caching
+{
+    /// <summary>
+    /// A service for caching downloaded songs
+    /// </summary>
+    public interface ISongCache
+    {
+        Task<bool> TryFindCachedSongAsync(SongId id, Action<Stream> result);
+        Task CacheSongAsync(ISong song);
+    }
+}
--- src/AudioChord/MusicServiceConfiguration.cs
+++ src/AudioChord/MusicServiceConfiguration.cs
@@ -1,7 +1,13 @@
-﻿namespace AudioChord
+﻿using AudioChord.Caching;
+using System;
+
+namespace AudioChord
 {
     public class MusicServiceConfiguration
     {
+        public Func<ISongCache> SongCacheFactory { get; set; }
+
+
         public string Username { get; set; }
         public string Password { get; set; }
         public string Hostname { get; set; } = "localhost";
