0e8bf9e08dc1f5bedd6f7ce7d0e7695e2c50112d
Lucas Spits <spits.lucas@gmail.com>
2019-01-25 14:54:26 +0100

    Add a new separate GridFS cache implementation

---
+++ src/AudioChord.Caching.GridFS/GridFSCache.cs
@@ -0,0 +1,86 @@
+﻿using MongoDB.Driver;
+using MongoDB.Driver.GridFS;
+using System;
+using System.IO;
+using System.Threading.Tasks;
+
+
+namespace AudioChord.Caching.GridFS
+{
+    /// <summary>
+    /// Caches songs to a mongodb GridFS Filesystem
+    /// </summary>
+    public class GridFSCache : ISongCache
+    {
+        const string BUCKET_NAME = "OpusData";
+
+        private GridFSCacheCleaner cleaner;
+
+        protected GridFSBucket<string> cache;
+
+        public GridFSCache(IMongoDatabase database)
+        {
+            cache = new GridFSBucket<string>(database, new GridFSBucketOptions()
+            {
+                BucketName = BUCKET_NAME,
+                ChunkSizeBytes = 4194304,
+
+                // We don't use MD5 in our code
+                DisableMD5 = true
+            });
+
+            cleaner = new GridFSCacheCleaner(cache);
+        }
+
+        /// <summary>
+        /// Cache the <see cref="ISong"/> in the GridFS cache if it doesn't already exist
+        /// </summary>
+        /// <param name="song">The <see cref="ISong"/> to try to cache</param>
+        public async Task CacheSongAsync(ISong song)
+        {
+            // Clean the cache first
+            await cleaner.CleanExpiredCacheEntries();
+
+            // We do not need to upload songs to the cache if they already exist
+            if(!DoesSongIdExist(song.Id))
+            {
+                await cache
+                    .UploadFromStreamAsync(
+                        id: song.Id.ToString(),
+                        filename: $"{song.Id}.opus",
+                        source: await song.GetMusicStreamAsync(),
+                        options: new GridFSUploadOptions() { Metadata = cleaner.GenerateGridFSMetadata() }
+                    );
+            }
+        }
+
+        public async Task<bool> TryFindCachedSongAsync(SongId id, Action<Stream> result)
+        {
+            // Check if we have the song in the cache
+            if (DoesSongIdExist(id))
+            {
+                //TODO: Are we gonna clean the cache here?
+
+                result(Stream.Synchronized(await cache.OpenDownloadStreamAsync(id.ToString())));
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        /// <summary>
+        /// Validate if the <see cref="SongId"/> Exists in the GridFS Cache
+        /// </summary>
+        /// <param name="id">The <see cref="SongId"/> to validate</param>
+        /// <returns>If the <see cref="SongId"/> exists in the GridFS Cache</returns>
+        private bool DoesSongIdExist(SongId id)
+        {
+            FilterDefinitionBuilder<GridFSFileInfo<string>> builder = new FilterDefinitionBuilder<GridFSFileInfo<string>>();
+            var definition = builder.Where(filter => filter.Id == id.ToString());
+
+            return cache.Find(definition).Any();
+        }
+    }
+}
---
+++ src/AudioChord.Caching.GridFS/GridFSCacheCleaner.cs
@@ -0,0 +1,59 @@
+﻿using MongoDB.Bson;
+using MongoDB.Driver;
+using MongoDB.Driver.GridFS;
+using System;
+using System.Threading.Tasks;
+
+namespace AudioChord.Caching.GridFS
+{
+    /// <summary>
+    /// Responsible for cleaning the GridFS Cache when needed
+    /// </summary>
+    internal class GridFSCacheCleaner
+    {
+        private GridFSBucket<string> cache;
+
+        protected IMongoCollection<GridFSFileInfo<string>> files;
+
+        /// <summary>
+        /// The name of the gridfs metadata key that contains the date when the song was last acessed
+        /// </summary>
+        private const string LAST_ACCESS_DATE_KEY = "lastAccessed";
+
+        public GridFSCacheCleaner(GridFSBucket<string> cache)
+        {
+            this.cache = cache;
+            files = cache.Database.GetCollection<GridFSFileInfo<string>>($"{cache.Options.BucketName}.files");
+        }
+
+        public async Task CleanExpiredCacheEntries()
+        {
+            FilterDefinitionBuilder<GridFSFileInfo<string>> builder = new FilterDefinitionBuilder<GridFSFileInfo<string>>();
+
+            // Find all the songs that haven't been used for 3 months
+            // TODO: How are we gonna upgrade all the songs?
+            // BACKCOMPAT: If the song does not have the last used date in the metadata then use the current date
+            FilterDefinition<GridFSFileInfo<string>> definition = builder
+                .Where(filter => filter.Metadata.GetValue(LAST_ACCESS_DATE_KEY, DateTime.Now) < DateTime.Now.AddMonths(-3));
+
+            // Delete a maximum of 100 songs per cleanup operation so we don't wait too long for returning a song
+            foreach(GridFSFileInfo<string> info in cache.Find(definition, new GridFSFindOptions<string>() { Limit = 100 }).ToEnumerable())
+            {
+                await cache.DeleteAsync(info.Id);
+            }
+        }
+
+        public void UpdateCacheTimestamp(SongId id)
+        {
+            var fieldDefinition = new StringFieldDefinition<GridFSFileInfo<string>, BsonDocument>($"metadata.{LAST_ACCESS_DATE_KEY}");
+
+            UpdateDefinitionBuilder<GridFSFileInfo<string>> builder = new UpdateDefinitionBuilder<GridFSFileInfo<string>>();
+            var updateCommand = builder.Set(fieldDefinition, GenerateGridFSMetadata());
+
+            files.UpdateOne(filter => filter.Id == id.ToString(), updateCommand);
+        }
+
+        public BsonDocument GenerateGridFSMetadata() 
+            => new BsonDocument(LAST_ACCESS_DATE_KEY, DateTime.UtcNow);
+    }
+}
