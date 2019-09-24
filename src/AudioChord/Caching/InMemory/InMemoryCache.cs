aab92fa500231a5f3e86d81a14ffd34373f48b3a
Lucas Spits <spits.lucas@gmail.com>
2019-09-24 17:18:09 +0200

    Implement in memory cache

--- src/AudioChord/Caching/InMemory/InMemoryCache.cs
+++ src/AudioChord/Caching/InMemory/InMemoryCache.cs
@@ -1,18 +1,27 @@
+using System.Collections.Generic;
 using System.IO;
 using System.Threading.Tasks;
 
 namespace AudioChord.Caching.InMemory
 {
+    /// <summary>
+    /// A Memory-based cache. This can be used for testing purposes or as a temporary solution
+    /// </summary>
     public class InMemoryCache : ISongCache
     {
+        private Dictionary<SongId, Stream> _cache = new Dictionary<SongId, Stream>();
+        
         public Task<(bool success, Stream result)> TryFindCachedSongAsync(SongId id)
         {
-            throw new System.NotImplementedException();
+            if (_cache.TryGetValue(id, out Stream stream))
+                return Task.FromResult((true, stream));
+
+            return Task.FromResult<(bool success, Stream result)>((false, Stream.Null));
         }
 
-        public Task CacheSongAsync(ISong song)
+        public async Task CacheSongAsync(ISong song)
         {
-            throw new System.NotImplementedException();
+            _cache.Add(song.Id, await song.GetMusicStreamAsync());
         }
     }
 }
