cecadde342b9e792c919f88bef4e67de680ef583
Lucas Spits <spits.lucas@protonmail.com>
2020-03-11 00:20:58 +0100

    Refactor: Use the constructor to create fresh extractors, add test descriptions

--- src/AudioChord.Tests/DiscordExtractorTests.cs
+++ src/AudioChord.Tests/DiscordExtractorTests.cs
@@ -10,36 +10,35 @@ namespace AudioChord.Tests
         private const string DISCORD_URL = "https://cdn.discordapp.com/attachments/400706177618673666/414561033370468352/Neptune.mp3";
         private const ulong ATTACHMENT_ID = 414561033370468352;
 
-        [Fact]
-        public void DiscordExtractorCanRetrieveId()
+        private readonly DiscordExtractor _extractor;
+
+        public DiscordExtractorTests()
         {
-            // Setup
-            DiscordExtractor extractor = new DiscordExtractor();
-            
-            // Assert
-            Assert.True(extractor.CanExtract(DISCORD_URL));
+            _extractor = new DiscordExtractor();
         }
 
-        [Fact]
+        [Fact(DisplayName = "Discord Extractor recognizes valid discord attachment Url")]
+        public void DiscordExtractorCanRetrieveId()
+        {
+            // Assert
+            Assert.True(_extractor.CanExtract(DISCORD_URL));
+        }
+
+        [Fact(DisplayName = "Discord Extractor generates the correct id from an Url")]
         public void DiscordExtractorRetrievesCorrectId()
         {
-            // Setup
-            DiscordExtractor extractor = new DiscordExtractor();
-
             // Assert
-            // This needs to be true
-            Assert.True(extractor.TryExtractSongId(DISCORD_URL, out SongId id));
+            Assert.True(_extractor.TryExtractSongId(DISCORD_URL, out SongId id));
             
             // The SongId source id should be the same as the attachment id
             Assert.Equal(ATTACHMENT_ID, ulong.Parse(id.SourceId));
         }
 
-        [Fact]
+        [Fact(DisplayName = "Discord Extractor can extract audio from a valid discord link")]
         public async Task DiscordExtractorCanExtractAudio()
         {
             // Setup
-            DiscordExtractor extractor = new DiscordExtractor();
-            ISong song = await extractor.ExtractAsync(DISCORD_URL, new ExtractorConfiguration());
+            ISong song = await _extractor.ExtractAsync(DISCORD_URL, new ExtractorConfiguration());
             
             // Assert
             Assert.NotNull(song);
