ffb6349c10226defdaa231b1e9c49594ae8ae102
Lucas Spits <spits.lucas@protonmail.com>
2020-05-07 01:26:03 +0200

    Tests: Add Tests for the discord media cdn

--- src/AudioChord.Tests/DiscordExtractorTests.cs
+++ src/AudioChord.Tests/DiscordExtractorTests.cs
@@ -26,14 +26,16 @@ namespace AudioChord.Tests
             Assert.True(_extractor.CanExtract(url) == expectedResult);
         }
 
-        [Fact(DisplayName = "Discord Extractor generates the correct id from an Url")]
-        public void DiscordExtractorRetrievesCorrectId()
+        [Theory(DisplayName = "Discord Extractor generates the correct id from an Url")]
+        [InlineData("https://media.discordapp.net/attachments/400706177618673666/707708192083410964/MultiGiftSub1.mp3", 707708192083410964)]
+        [InlineData("https://cdn.discordapp.com/attachments/400706177618673666/414561033370468352/Neptune.mp3", 414561033370468352)]
+        public void DiscordExtractorRetrievesCorrectId(string url, ulong expectedId)
         {
             // Assert
-            Assert.True(_extractor.TryExtractSongId(DISCORD_URL, out SongId id));
+            Assert.True(_extractor.TryExtractSongId(url, out SongId id));
             
             // The SongId source id should be the same as the attachment id
-            Assert.Equal(ATTACHMENT_ID, ulong.Parse(id.SourceId));
+            Assert.Equal(expectedId, ulong.Parse(id.SourceId));
         }
 
         [Fact(DisplayName = "Discord Extractor can extract audio from a valid discord link")]
@@ -45,6 +47,7 @@ namespace AudioChord.Tests
             // Assert
             Assert.NotNull(song);
             Assert.Equal(song.Metadata.Url, DISCORD_URL);
+            // TODO: Test if the file is an opus file
         } 
     }
 }
